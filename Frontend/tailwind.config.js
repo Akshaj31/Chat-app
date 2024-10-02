cc8b3ac9dcd9dd85548667faf558be0a6611f0f7
Akshaj <akshajpaintola@gmail.com>
2024-10-02 22:50:18 +0530

    Add Poppins font import and extend font family in Tailwind CSS configuration

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'sans': ['Poppins', 'ui-sans-serif', 'system-ui'],
+      }
+    },
   },
   plugins: [],
 }
